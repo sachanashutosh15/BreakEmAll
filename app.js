213dbef409773e15813cb3b692dc79c29622a78c
sachanashutosh15 <sachanashutosh15@gmail.com>
2022-07-14 20:41:46 +0530

    did some folder restructuring and made express app

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+	res.sendFile('public/index.html');
+})
+
+app.listen(3000, () => console.log('Server is running...'))
