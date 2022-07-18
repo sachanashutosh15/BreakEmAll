8a3808e4c6e580b243faab8c80f80c2fd3454f0e
sachanashutosh15 <sachanashutosh15@gmail.com>
2022-07-18 07:07:34 +0530

    update port in app.js

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ import express from 'express';
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.static('src'));
 
 app.get('/', function(req, res) {
 	res.sendFile('public/index.html');
 })
 
-app.listen(3000, () => console.log('Server is running...'))
+app.listen(process.env.PORT || 3000, () => console.log('Server is running...'))
